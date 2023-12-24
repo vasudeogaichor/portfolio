c9d5a6678659be25537a797e403ce68e59093312
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-12-24 23:40:32 +0530

    added layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import Header from '@/components/header'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -15,12 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-grey-50 text-grey-950`}>
-        <div className='bg-[#00000] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
-        <div className='bg-[#dbd7fd] absolute top-[-1rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
-        <div></div>
+      <body className={`${inter.className} bg-grey-50 text-grey-950 relative h-[5000px]`}>
+        <div className='bg-[#fbe2e3] -z-10 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
+        <div className='bg-[#dbd7fd] -z-10 absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+        <Header />
         {children}
-        </body>
+      </body>
     </html>
   )
 }
