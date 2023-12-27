648e455c5bf1db2f27fdd78a5d486f58ed82b36d
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-12-27 12:25:05 +0530

    added about me section

---
+++ components/about.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React from 'react'
+import SectionHeading from './section-heading'
+import { motion } from 'framer-motion';
+
+export default function About() {
+    return (
+        <motion.section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+        >
+            <SectionHeading>About Me</SectionHeading>
+            <p className='mb-3'>
+                After graduating with a degree in{" "}
+                <span className="font-medium">Mathematics</span>, I decided to pursue my
+                passion for programming. I enrolled in a coding bootcamp and learned{" "}
+                <span className="font-medium">full-stack web development</span>.{" "}
+                <span className="italic">My favorite part of programming</span> is the
+                problem-solving aspect. I <span className="underline">love</span> the
+                feeling of finally figuring out a solution to a problem. My core stack
+                is{" "}
+                <span className="font-medium">
+                    React, Next.js, Node.js, and MongoDB
+                </span>
+                . I am also familiar with TypeScript, python and relational databases. I am always looking to
+                learn new technologies. I am currently looking for a{" "}
+                <span className="font-medium">full-time position</span> as a software
+                developer.
+            </p>
+
+            <p>
+                <span className="italic">When I'm not coding</span>, I enjoy working out, watching movies, listening to podcasts. I also enjoy{" "}
+                <span className="font-medium">working on my own projects</span>. I am currently
+                working on a {" "}
+                <span className="font-medium">blogs project</span>.
+            </p>
+        </motion.section>
+    )
+}
